503b4073c5f3a01873873f6d110ffb272c495114
özkan demir <ozkanndemir2009@gmail.com>
2022-10-13 14:46:57 +0300

    last app

--- app2.js
+++ app2.js
@@ -1,22 +1,40 @@
-const poli = (a, b) => {
-  let result = []
-    for (let i = a; i <= b; i++) {
-      
-        if (i < 10) {
-             console.log(i);
-             result.push(i)
-        } else {
-            let x = i.toString().split("").reverse().join("")
-            console.log(x);
-            if(x==i){
-                result.push(+x)
-
-
-            }
+// const poli = (a, b) => {
+//   let result = []
+    // for (let i = a; i <= b; i++) {
+    //   
+        // if (i < 10) {
+            //  console.log(i);
+            //  result.push(i)
+        // } else {
+            // let x = i.toString().split("").reverse().join("")
+            // console.log(x);
+            // if(x==i){
+                // result.push(+x)
+// 
+// 
+            // }
+        // }
+//  
+    // }
+//    return result
+// 
+// };
+    // console.log(poli(0, 500));
+const perfectNumber = (num) => {
+    console.log(num);
+    let count = 0;
+    console.log(count);
+    for (let i = 1; i <num; i++) {
+        if (num % i == 0) {
+            console.log(num)
+            count += i
         }
- 
     }
-   return result
+    if (count == num){
+        return num
+    }else{
+        return 
+    }
 
-};
-    console.log(poli(0, 500));
+}
+console.log(perfectNumber(28));
